d6b2d3e0fdd66fca01c8b4bd9675a942215a7ed3
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2022-01-13 11:26:47 -0500

    Create keepassxc.sh

---
+++ keepassxc/keepassxc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+version=2.6.6
+
+rm -rf *.tar.gz
+
+rm -rf keepassxc-*/
+
+wget https://github.com/keepassxreboot/keepassxc/archive/refs/tags/${version}.tar.gz -O keepassxc-${version}.tar.gz
+
+tar -xvf keepassxc-${version}.tar.gz
+
+cp debian keepassxc-${version}/
+
+cd keepassxc-${version}
+
+sudo mk-build-deps \
+    --tool \
+    'apt-cudf-get --solver aspcud -o APT::Get::Assume-Yes=1 -o Debug::pkgProblemResolver=0 -o APT::Install-Recommends=0' \
+    -i debian/control
+
+dpkg-buildpackage -us -uc -nc
+
+cd ../
